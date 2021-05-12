8c485eeec87c006d2abd196a82be131ccce697d2
Patrick Lago <Patrick@Dinas-Air.attlocal.net>
2021-05-12 15:47:52 -0700

    generated HMTL from app

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-    console.log('=======');
-
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};  
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
